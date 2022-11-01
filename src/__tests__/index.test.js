742a239fde8fb574ddb0d6f07f4eb940b9ea904a
Toby Pouler <tpouler@middlebury.edu>
2022-10-31 22:00:13 -0400

    Add more styling

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,13 +1,11 @@
-
 import { render, screen } from "@testing-library/react";
 
 import Main from "../pages/index";
 
-
 /**
  * This is a placeholder test that is here only to make sure `npm test` will not choke because there are no tests. It should be removed as soon as you write a real test.
  */
-test("Placeholder test - replace with real tests", ()=>{
-    render(<Main />);
-    expect(screen.getByText("Generic Project")).toBeVisible();
+test("Placeholder test - replace with real tests", () => {
+  render(<Main />);
+  expect(screen.getByText("Quiz 1")).toBeVisible();
 });
