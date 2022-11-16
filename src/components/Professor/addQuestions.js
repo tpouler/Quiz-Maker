4af936e887d922aa0e87b7915f4e488a13388ddc
Alivia Kliesen <askliesen@gmail.com>
2022-11-15 20:05:11 -0500

    linking prof to database

--- src/components/Professor/addQuestions.js
+++ src/components/Professor/addQuestions.js
@@ -1,16 +1,30 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 import "react-awesome-button/dist/styles.css";
+import { addQuestion } from "../../utils/firebase-utils.mjs";
+import { useEffect } from "react";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
 export default function AddQuestion({ topics, setSubmitted }) {
   // eslint-disable-line
 
   const [question, setQuestion] = useState(); // eslint-disable-line
   const [answer, setAnswer] = useState(); // eslint-disable-line
+  const [topic, setTopic] = useState();
+  const [newTopic, setNewTopic] = useState(false);
 
-  const topicsList = topics.map((topic) => (
-    <option id={topic} key={topic}>
+  function handleChange(e) {
+    if (e.target.value === "custom") {
+      setNewTopic(true);
+    } else {
+      setNewTopic(false);
+      setTopic(e.target.value);
+    }
+  }
+
+  const topicsList = topics.map((t) => (
+    <option id={t} key={t} value={t}>
       {" "}
-      {topic}{" "}
+      {t}{" "}
     </option>
   ));
 
@@ -37,13 +51,48 @@ export default function AddQuestion({ topics, setSubmitted }) {
           />
         </p>
         <label htmlFor="topics"> Select a topic: </label>
-        <select name="topics" id="topics">
+        <select name="topics" id="topics" onChange={handleChange}>
           {topicsList}
-          <option id="custom"> Add a new category </option>
+          <option id="custom" value="custom">
+            {" "}
+            Add a new category{" "}
+          </option>
         </select>
+        {newTopic && (
+          <input
+            required
+            type="NewTopic"
+            placeholder="Write new topic name here"
+            onChange={(event) => {
+              setTopic(event.target.value);
+            }}
+          />
+        )}
       </div>
     );
   };
+
+  async function createID(inputTopic) {
+    const db = getFirestore();
+    let id = 0;
+    if (!newTopic) {
+      const topicSnapshot = await getDocs(collection(db, inputTopic));
+      const topicsArr = [];
+      topicSnapshot.forEach((document) => {
+        topicsArr.push({ ...document.data() });
+      });
+
+      let greatestID = 0;
+      for (let i = 0; i < topicsArr.length; i++) {
+        if (topicsArr[i].id > greatestID) {
+          greatestID = topicsArr[i].id;
+        }
+      }
+
+      id = greatestID + 1;
+    }
+    return id.toString();
+  }
 
   const submit = () => {
     if (
@@ -52,9 +101,27 @@ export default function AddQuestion({ topics, setSubmitted }) {
       (answer !== undefined) &
       (answer !== "")
     ) {
+      console.log("topic:");
+      console.log(topic);
+      id = createID(topic);
+
+      const questionObj = {
+        id: "7",
+        question: question,
+        answer: answer,
+        response: "",
+        topic: topic,
+      };
+
+      addQuestion(questionObj);
+
       setSubmitted(true);
     }
   };
+
+  useEffect(() => {
+    console.log(newTopic);
+  }, [newTopic]);
 
   return (
     <div>
