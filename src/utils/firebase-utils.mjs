e56323114c6134010d445b785d3af838a1d1c24f
Alivia Kliesen <akliesen@middlebury.edu>
2022-11-30 17:45:56 -0500

    keep track of category scores and better results page

--- src/utils/firebase-utils.mjs
+++ src/utils/firebase-utils.mjs
@@ -63,7 +63,6 @@ export async function addQuestion(question) {
 
   //console.log("Here testing");
   const copy = JSON.parse(JSON.stringify(question));
-  delete copy.topic;
 
   const sectionsRef = collection(db, "questions");
 
@@ -86,8 +85,7 @@ export async function addQuestion(question) {
 
 export async function addResult(result, uid){
   const currDate = new Date();
-  const resultData = {
-    score: result.score,
+  const resultData = { ...result,
     date: currDate.toLocaleString(),
   };
   const db = getFirestore();
