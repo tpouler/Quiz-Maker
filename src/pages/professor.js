41ac3628a3397f6f456011029c06aebd8285ddd9
Alivia Kliesen <akliesen@middlebury.edu>
2022-12-03 13:08:25 -0500

    changed firestore rules date and cleaned up ProfessorMain

--- src/pages/professor.js
+++ src/pages/professor.js
@@ -12,91 +12,62 @@ import questionFill from "@iconify/icons-akar-icons/question-fill";
 import quizIcon from "@iconify/icons-material-symbols/quiz";
 import useTopics from "../hooks/useTopics";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function ProfessorMain() {
   const [submitted, setSubmitted] = useState();
 
   const topicsList = useTopics();
 
-  if (submitted !== true) {
-    return (
-      <div className={styles.header}>
-        <Head>
-          <title>Potoo project</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+  useEffect(() => {}, [submitted]); // eslint-disable-line
 
-        <div className={styles.topnav}>
-          <Link href="/">
-            <span>
-              <Icon icon={homeAlt1} width="25" height="25" inline />
-              Home
-            </span>
-          </Link>
-          <Link href="/professor">
-            <span className={styles.active}>
-              <Icon icon={questionFill} width="25" height="20" inline />
-              Professor
-            </span>
-          </Link>
-          <Link href="/quiz">
-            <span>
-              <Icon icon={quizIcon} width="25" height="20" inline />
-              Quiz
-            </span>
-          </Link>
-          <LoginStatus />
-        </div>
+  return (
+    <div className={styles.header}>
+      <Head>
+        <title>Potoo project</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main>
-          <h1 className="title">Add Questions</h1>
+      <div className={styles.topnav}>
+        <Link href="/">
+          <span>
+            <Icon icon={homeAlt1} width="25" height="25" inline />
+            Home
+          </span>
+        </Link>
+        <Link href="/professor">
+          <span className={styles.active}>
+            <Icon icon={questionFill} width="25" height="20" inline />
+            Professor
+          </span>
+        </Link>
+        <Link href="/quiz">
+          <span>
+            <Icon icon={quizIcon} width="25" height="20" inline />
+            Quiz
+          </span>
+        </Link>
+        <LoginStatus />
+      </div>
+
+      <main>
+        <h1 className="title">Add Questions</h1>
+        {!submitted && (
           <AddQuestion
             topics={topicsList}
             setSubmitted={setSubmitted}
             submitted={submitted}
           />
-        </main>
-
-        <footer>A 312 project</footer>
-      </div>
-    );
-  } else {
-    return (
-      <div className={styles.header}>
-        <Head>
-          <title>Potoo project</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className={styles.topnav}>
-          <Link href="/">
-            <span>
-              <Icon icon={homeAlt1} width="25" height="25" inline />
-              Home
-            </span>
-          </Link>
-          <Link href="/professor">
-            <span className={styles.active}>
-              <Icon icon={questionFill} width="25" height="20" inline />
-              Professor
-            </span>
-          </Link>
-          <Link href="/quiz">
-            <span>
-              <Icon icon={quizIcon} width="25" height="20" inline />
-              Quiz
-            </span>
-          </Link>
-          <LoginStatus />
-        </div>
-        <main>
-          <h1 className="title">Add Questions</h1>
+        )}
+        {submitted && (
           <SubmittedQuestions
             setSubmitted={setSubmitted}
             submitted={submitted}
           />
-        </main>
-        <footer>A 312 project</footer>
-      </div>
-    );
-  }
+        )}
+      </main>
+
+      <footer>A 312 project</footer>
+    </div>
+  );
 }
