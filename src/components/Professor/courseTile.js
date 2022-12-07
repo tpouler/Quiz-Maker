867d866e9df5d5c26ea5f96d645abd244f6cc1a7
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2022-12-07 14:37:27 -0500

    course page update

--- src/components/Professor/courseTile.js
+++ src/components/Professor/courseTile.js
@@ -1,10 +1,13 @@
 import style from "../../styles/manage.module.css";
-import inputStyle from "../../styles/inputBox.module.css";
+//import inputStyle from "../../styles/inputBox.module.css";
+import styles from "../../styles/index.module.css";
 import Collapsible from "react-collapsible";
 import PropTypes from "prop-types";
 import useStudents from "../../hooks/useStudents";
 import { addStudent, removeStudent } from "../../utils/firebase-utils.mjs";
 import { useState } from "react";
+import "react-awesome-button/dist/styles.css";
+import { AwesomeButton } from "react-awesome-button";
 
 export default function CourseTile({ course }) {
   const [email, setEmail] = useState();
@@ -13,7 +16,10 @@ export default function CourseTile({ course }) {
   const studentList = students.map((s) => (
     <li className={style.li} key={s}>
       {s} {"    "}
-      <button onClick={() => removeStudent(course, s)}> remove </button>
+      <button className={style.right} onClick={() => removeStudent(course, s)}>
+        {" "}
+        remove{" "}
+      </button>
     </li>
   ));
 
@@ -24,13 +30,22 @@ export default function CourseTile({ course }) {
           {studentList}
           <li className={style.li} key="add">
             <input
-              placeholder="Enter student email"
+              type="text"
+              // value={answer}
               onChange={(event) => {
                 setEmail(event.target.value);
               }}
-              className={inputStyle.input}
+              placeholder="Enter student email"
+              className={styles.inputProf}
             />
-            <button onClick={() => addStudent(course, email)}> add </button>
+            <div className={styles.divider} />
+            {/* <button onClick={() => addStudent(course, email)}> add </button> */}
+            <AwesomeButton
+              type="secondary"
+              onReleased={() => addStudent(course, email)}
+            >
+              Add
+            </AwesomeButton>
           </li>
         </ul>
       </Collapsible>
