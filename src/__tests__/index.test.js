fd3408ca2cc10eac66081d8da028aba61f2a2988
Alivia Kliesen <akliesen@middlebury.edu>
2022-12-08 11:45:25 -0500

    only student results saved

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import Main from "../pages/index";
 
 /**
- * This is a placeholder test that is here only to make sure `npm test` will not choke because there are no tests. It should be removed as soon as you write a real test.
+ * This is a placehold test that is here only to make sure `npm test` will not choke because there are no tests. It should be removed as soon as you write a real test.
  */
 describe("PLACEHOLDER", () => {
   test("main app render", () => {
